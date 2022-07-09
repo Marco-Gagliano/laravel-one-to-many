9ea0fb733cdd4cbded6c9a28dbd41ab1d45765a9
Marco-Gagliano <brazil_94@hotmail.it>
2022-07-09 15:35:06 +0200

    inserito-model-migration-seeder

---
+++ app/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    // $data =  ['HTML', 'CSS', 'Javascript', 'PHP', 'C++', 'Bootstrap', 'Laravel'];
+
+    // foreach ($data as $key => $value) {
+
+    // }
+}
