39235353db79cf25eb4168367b53f47286d1c324
Marco-Gagliano <brazil_94@hotmail.it>
2022-07-09 16:09:14 +0200

    creato-e-popolato-la-FK

---
+++ database/migrations/2022_07_09_140009_update_posts_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            //1. Creo la colonna della Foreign key
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+
+	        //2. Assegno la foreign key alla colonna creata
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('set null');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+
+            //1. Elimino la foreign key
+            $table->dropForeign(['category_id']);
+
+            //2. Elimino la colonna
+            $table->dropColumn('category_id');
+        });
+
+    }
+}
